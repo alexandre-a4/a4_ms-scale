e533331b6d5826ed87db2e76a6a59967aa109b6e
alexandre-a4 <alexandre.lamulle@gmail.com>
2026-05-05 16:57:52 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+input.onButtonPressed(Button.A, function () {
+    HX711WeightSensor.tare()
+})
+HX711WeightSensor.initialize()
+HX711WeightSensor.tare()
+lcdDisplay.lcdInitIIC()
+lcdDisplay.lcdClearAll()
+lcdDisplay.lcdSetBgcolor(0xb09eff)
+Keypad4x4.initialize()
+basic.forever(function () {
+    lcdDisplay.lcdDisplayText("" + Math.round(HX711WeightSensor.weightInGrams()) + "gr", 1, 120, 120, lcdDisplay.FontSize.Large, 0x000000)
+    lcdDisplay.lcdDisplayText(Keypad4x4.pressedKey(), 2, 120, 145, lcdDisplay.FontSize.Large, 0x000000)
+    basic.pause(500)
+})
